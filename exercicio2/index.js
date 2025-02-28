86cb7b47c53c0b7e4610f407ab9651cf3eb7848d
Gustavo-Simoes <145218405+Gustavo-Simoes@users.noreply.github.com>
2025-02-28 13:32:57 -0300

    Opercaoes Basicas

---
+++ exercicio2/index.js
@@ -0,0 +1,23 @@
+let valor1 = parseInt(prompt("Digite o primeiro numero: "))
+let valor2 = parseInt(prompt("Digite o segundo numero: "))
+
+let soma = valor1 + valor2 
+let subtracao = valor1 - valor2
+let divisao = valor1 * valor2
+let multiplicacao = valor1 / valor2
+
+
+window.alert(
+    "Resultado da operacao de adicao: " + (valor1 + valor2) + 
+    "\n" +
+
+    "Resultado da operacao de subtracao: " + (valor1 - valor2) + 
+    "\n" +
+
+    "Resultado da operacao de divisao: " + (valor1 / valor2) + 
+    "\n" +
+
+    "Resultado da operacao de multiplicacao: " + (valor1 * valor2) 
+    
+
+)
