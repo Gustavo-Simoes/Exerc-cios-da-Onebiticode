866e1e59a0ca47a53c924c601b021983cb5ba740
Gustavo-Simoes <145218405+Gustavo-Simoes@users.noreply.github.com>
2025-03-07 15:58:23 -0300

    alteracoes apos ver a correcao

--- exercicio1/index.js
+++ exercicio1/index.js
@@ -1,13 +1,13 @@
 let nome = prompt("Digite seu primeiro nome");
 let sobrenome = prompt("Digite seu segundo nome");
 let campo = prompt("Digite o seu campo de estudo");
-let num = parseInt(prompt("Digite seu ano de nascimento"));
+let anoDeNascimento = prompt("Digite seu ano de nascimento")
 
-let date = new Date().getFullYear(); 
-let idade = date - num;
+
 
 window.alert(
+    "Recruta cadastrado com sucesso!" + "\n" +
     "Nome completo do sodaldo: " + nome + " " + sobrenome + "\n" +
     "Campo de estudo: " + campo + "\n" +
-    "Idade: " + idade + " anos"
+    "Idade: " + (2025 - anoDeNascimento) + " anos"
 );
